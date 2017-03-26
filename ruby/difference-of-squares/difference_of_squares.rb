d56e071039a77baa491da90238835e3bc6f2b9fc
Curtis Branum <curtis.branum@wheniwork.com>
2017-03-26 13:46:50 -0500

    difference of sqaures

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,1 +1,21 @@
 module BookKeeping
+  VERSION = 3
+end
+
+class Squares
+  def initialize(value)
+    @range = 1..value
+  end
+
+  def square_of_sum
+    @range.sum ** 2
+  end
+
+  def sum_of_squares
+    (@range.map {|n| n ** 2}).sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
